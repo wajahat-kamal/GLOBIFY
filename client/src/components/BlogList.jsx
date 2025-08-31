7240f4dde7ac3c7cca194128991d62502e4659d6
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-08-31 10:46:15 +0500

    Update BlogList component button styling for improved visual consistency and add tslib dependency in package-lock.json

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -7,15 +7,14 @@ function BlogList() {
   return (
     <section className="w-full px-4 md:px-10 py-6 pt-8 bg-white">
       {/* Category Buttons */}
-      <div className="flex flex-wrap items-center justify-center md:gap-4 gap-2 mb-10">
+      <div className="flex flex-wrap items-center justify-center md:gap-6 gap-2 mb-10">
         {blogCategory.map((item) => (
           <button
             key={item}
             onClick={() => setMenu(item)}
-            className={`px-2 md:px-4 py-1 rounded-4xl text-sm md:text-lg font-medium 
-                       bg-gray-100 text-gray-500 
-                      ${menu === item && "bg-primary text-white"}
-                       shadow-sm transition-all duration-300`}
+            className={`relative px-5 py-1 text-sm md:text-[17px] font-medium rounded-4xl overflow-hidden
+                       bg-gray-100 text-gray-500 transition-all duration-300
+                       ${menu === item ? "bg-indigo-600 text-white" : "hover:bg-gray-200"}`}
           >
             {item}
           </button>
