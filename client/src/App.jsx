32858cdd7eeda55f722f996ebf8fc469c30a5f5f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-03 17:04:41 +0500

    Add admin routes for dashboard, blog management, and comments in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,27 @@
 import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import BlogList from "./pages/admin/BlogList";
+import Comments from "./pages/admin/Comments";
+import AddBlog from "./pages/admin/AddBlog";
 
 function App() {
   return (
     <div>
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="blog-list" element={<BlogList />} />
+          <Route path="comments" element={<Comments />} />
+          <Route path="add-blog" element={<AddBlog />} />
+        </Route>
       </Routes>
     </div>
   );
