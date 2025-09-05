95cad23b7faa0222addc18598944051d8c74c828
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-05 07:18:33 +0500

    Refactor BlogList and Comments components to enhance layout and user experience with improved styling and descriptive headings.

--- client/src/pages/admin/BlogList.jsx
+++ client/src/pages/admin/BlogList.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function BlogList() {
+function BlogLists() {
   return (
-    <div>BlogList</div>
-  )
+    <div className= rounded-xl shadow-sm p-6">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">📑 Blog Lists</h1>
+      <p className="text-gray-600">
+        Here’s a list of all your blogs. You can edit or delete them anytime.
+      </p>
+    </div>
+  );
 }
 
-export default BlogList
+export default BlogLists;
--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function Comments() {
   return (
-    <div>Comments</div>
-  )
+    <div className=" rounded-xl shadow-sm p-6">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">💬 Comments</h1>
+      <p className="text-gray-600">
+        Manage all the comments on your blog posts from this page.
+      </p>
+    </div>
+  );
 }
 
-export default Comments
+export default Comments;
