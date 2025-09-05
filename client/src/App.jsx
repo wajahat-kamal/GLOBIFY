cd2832e8504a3076842faa6ec5fddd8836d4c979
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-05 07:47:10 +0500

    Update admin route in App component to always render Layout, and enhance Dashboard component styling for improved layout and user experience.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/blog/:id" element={<Blog />} />
 
         {/* Admin Routes */}
-        <Route path="/admin" element={false ? <Layout /> : <Login/>}>
+        <Route path="/admin" element={true ? <Layout /> : <Login/>}>
           <Route index element={<Dashboard />} />
           <Route path="add-blogs" element={<AddBlog />} />
           <Route path="blog-lists" element={<BlogList />} />
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div className=" rounded-xl shadow-sm p-6">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4"> Dashboard</h1>
-      <p className="text-gray-600">
-        Welcome to your dashboard! Here you can see a quick overview of your blog stats.
-      </p>
+    <div className="px-6 py-3 w-full">
+      <div>
+        <h1 className="text-2xl font-bold text-gray-800 mb-2"> Dashboard</h1>
+        <p className="text-gray-600">
+          Welcome to your dashboard! Here you can see a quick overview of your
+          blog stats.
+        </p>
+      </div>
     </div>
   );
 }
