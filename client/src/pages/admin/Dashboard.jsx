6eb9a609241c86f14260d7575ecb9e59cb402a81
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-05 07:52:28 +0500

    Refactor Dashboard component to replace static content with dynamic blog stats display, enhancing layout and user experience.

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,15 +1,34 @@
+import { MessageSquare } from "lucide-react";
 import React from "react";
 
 function Dashboard() {
   return (
     <div className="px-6 py-3 w-full">
-      <div>
-        <h1 className="text-2xl font-bold text-gray-800 mb-2"> Dashboard</h1>
-        <p className="text-gray-600">
-          Welcome to your dashboard! Here you can see a quick overview of your
-          blog stats.
-        </p>
+      <div className="flex flex-col md:flex-row items-center justify-start">
+        <div className="bg-white px-14 py-10 flex flex-row items-center justify-center">
+          {/* Lucide-react icon */}
+          <div>
+            <h1>12</h1>
+            <p>Blogs</p>
+          </div>
+        </div>
+        <div className="bg-white p-6 flex flex-row items-center justify-center">
+          {/* Lucide-react icon */}
+          <div>
+            <h1>6</h1>
+            <p>Comments</p>
+          </div>
+        </div>
+        <div className="bg-white p-6 flex flex-row items-center justify-center">
+          {/* Lucide-react icon */}
+          <div>
+            <h1>0</h1>
+            <p>Drafts</p>
+          </div>
+        </div>
       </div>
+
+
     </div>
   );
 }
