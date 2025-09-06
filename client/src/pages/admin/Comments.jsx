bfc047471dd35d3f7ab27bd17870fca08aa2e79f
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-06 10:13:58 +0500

    Update Comments component to implement filtering buttons for 'Approved' and 'Not Approved' comments, enhancing user interaction and management capabilities.

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -2,28 +2,41 @@ import React, { useEffect, useState } from "react";
 import { commentsData } from "../../assets/blogsData";
 
 function Comments() {
-
-  const [comments, setComments] = useState([])
-  const [filter, setFilter] = useState([])
+  const [comments, setComments] = useState([]);
+  const [filter, setFilter] = useState("Not Approved");
 
   const fetchComments = async () => {
-    setComments(commentsData)
-  }
+    setComments(commentsData);
+  };
 
   useEffect(() => {
-    fetchComments()
-  }, [])
-  
+    fetchComments();
+  }, []);
 
   return (
     <div>
+      <div>
+        <h1 className="text-2xl font-bold text-gray-800 mb-4">Comments</h1>
+        <div>
+          <button
+            className={`border border-gray-700 rounded-full text-xs py-1.5 px-3 ${
+              filter === "Approved" ? "text-primary" : "text-gray-700"
+            }`}
+          >
+            Approved
+          </button>
+          <button
+            className={`border border-primary rounded-full text-xs py-1.5 px-3 ${
+              filter === "Not Approved" ? "text-primary" : "text-gray-700"
+            }`}
+          >
+            Not Approved
+          </button>
+        </div>
+      </div>
 
-    <div>
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">Comments</h1>
-      <p className="text-gray-600">
-        Manage all the comments on your blog posts from this page.
-      </p>
-    </div>
+      {/* comments */}
+      <div></div>
     </div>
   );
 }
