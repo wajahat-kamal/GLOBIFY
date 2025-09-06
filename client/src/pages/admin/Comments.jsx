cf734bb0b638cb06ce617bc429c11e00051e9aa9
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-06 09:42:12 +0500

    Implement comments fetching in Comments component with useState and useEffect hooks for improved data management

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { commentsData } from "../../assets/blogsData";
 
 function Comments() {
+
+  const [comments, setComments] = useState([])
+  const [filter, setFilter] = useState([])
+
+  const fetchComments = async () => {
+    setComments(commentsData)
+  }
+
+  useEffect(() => {
+    fetchComments()
+  }, [])
+  
+
   return (
     <div>
 
-    <div className="p-6">
+    <div>
       <h1 className="text-2xl font-bold text-gray-800 mb-4">Comments</h1>
       <p className="text-gray-600">
         Manage all the comments on your blog posts from this page.
