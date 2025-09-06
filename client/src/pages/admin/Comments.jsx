e7aeb607a251a3b54bc7e54fa5986cca9cf63a34
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-06 09:39:43 +0500

    Add icon to Comments header for improved visual appeal

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -2,11 +2,14 @@ import React from "react";
 
 function Comments() {
   return (
-    <div className=" rounded-xl shadow-sm p-6">
-      <h1 className="text-2xl font-bold text-gray-800 mb-4">💬 Comments</h1>
+    <div>
+
+    <div className="p-6">
+      <h1 className="text-2xl font-bold text-gray-800 mb-4">Comments</h1>
       <p className="text-gray-600">
         Manage all the comments on your blog posts from this page.
       </p>
+    </div>
     </div>
   );
 }
