d228f4cb52d500ee275137c3488112f0311d9974
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-08 17:18:05 +0500

    Add user signup functionality by creating a new user record in the database after password hashing.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,15 +1,37 @@
 import jwt from "jsonwebtoken";
-
+import User from "../models/user.model";
 
 export const signup = async (req, res) => {
-    const  {fullName, email, password} = req.body;
-  
+  try {
+    const { fullName, email, password } = req.body;
+
     if (!fullName || !email || !password) {
       return res.status(404).json({
         success: false,
-        message: "All fields are required"
-      })
+        message: "All fields are required",
+      });
     }
 
-   const hashPassword = await bcrypt.hash(password, 10)
-}
+    if (condition) {
+      
+    }
+
+    const hashPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      fullName,
+      email,
+      password: hashPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "",
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+    });
+  }
+};
