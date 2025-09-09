f90b50c452812004eb0239e2348372a352c54a48
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-09 16:15:47 +0500

    Update Navbar component to display user avatar and name when logged in, enhancing user experience and interface consistency.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { LogIn } from "lucide-react";
 import logo from "../assets/globify-logo.png";
 import { useNavigate } from "react-router-dom";
+import avatar from "../assets/user-avatar.png";
 
 export default function Navbar() {
   const navigate = useNavigate();
@@ -26,17 +27,29 @@ export default function Navbar() {
           </div>
 
           {/* Right: Auth button */}
-          <button
-            onClick={() => {
-              false ? navigate("/admin") : navigate("/login");
-            }}
-            className="inline-flex items-center justify-center gap-2 md:px-6 px-4 py-3 rounded-full text-sm font-medium
-             bg-primary text-white shadow-md hover:bg-primary/90 hover:shadow-lg
-             transition-all duration-300 min-w-[140px]"
-          >
-            <span>Login / Signup</span>
-            <LogIn className="h-4 w-4" />
-          </button>
+
+          {true ? (
+            <div className="flex items-center gap-2 pl-2 pr-6 py-1.5 rounded-full bg-primary shadow-lg hover:shadow-xl transition-all duration-300 cursor-pointer">
+              <img
+                src={avatar}
+                alt="Avatar"
+                className="w-9 h-9 rounded-full border border-white/20 shadow-md object-cover"
+              />
+              <span className="font-semibold text-white text-sm md:text-base tracking-wide">
+                Wajahat Kamal
+              </span>
+            </div>
+          ) : (
+            <button
+              onClick={() => navigate("/login")}
+              className="inline-flex items-center justify-center gap-2 md:px-6 px-4 py-3 rounded-full text-sm font-medium
+                     bg-primary text-white shadow-md hover:bg-primary/90 hover:shadow-lg
+                     transition-all duration-300 min-w-[140px]"
+            >
+              <span>Login / Signup</span>
+              <LogIn className="h-4 w-4" />
+            </button>
+          )}
         </nav>
       </div>
     </header>
