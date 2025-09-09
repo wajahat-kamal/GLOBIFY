28fce5a8e9ff59554b3f6c32c1192536eeb14e1c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-09 12:52:21 +0500

    Add login functionality to userController, including JWT token generation and error handling, and update user routes to include login endpoint.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res) => {
   try {
@@ -49,6 +50,63 @@ export const signup = async (req, res) => {
     });
   } catch (error) {
     console.error("Signup error:", error.message);
+    return res.status(500).json({
+      success: false,
+      message: "Server error, please try again later",
+    });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "Incorrect email or password",
+      });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+
+    return res
+      .status(200)
+      .cookie("token", token, {
+        maxAge: 1 * 24 * 60 * 60 * 1000, 
+        httpOnly: true,
+        sameSite: "strict",
+      })
+      .json({
+        success: true,
+        message: `Welcome back ${user.fullName}`,
+        user: {
+          id: user._id,
+          fullName: user.fullName,
+          email: user.email,
+        },
+        token,
+      });
+  } catch (error) {
+    console.error("Login error:", error.message);
     return res.status(500).json({
       success: false,
       message: "Server error, please try again later",
