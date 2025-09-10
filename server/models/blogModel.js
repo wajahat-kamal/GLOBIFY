534d70cfa70782e58b2ae176e27491c03a3ad22c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-10 11:02:50 +0500

    Refactor server.js to enhance API structure by integrating admin routes and removing the .gitignore file for improved project organization.

---
+++ server/models/blogModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    image: {
+      type: String,
+      default: "", 
+    },
+    category: {
+      type: String,
+      default: "Startup", 
+      trim: true,
+    },
+    isPublish: {
+      type: Boolean,
+      default: false, 
+    },
+  },
+  { timestamps: true } 
+);
+
+const Blog = mongoose.model("blog", blogSchema);
+export default Blog;
