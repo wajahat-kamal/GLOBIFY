9f99c45a2dd917223ec8e2fbdff4198d51e171bb
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-10 12:46:51 +0500

    Integrate image upload functionality in blog route
    - Updated the POST route for adding blogs to include image upload capability using multer middleware.
    - Ensured that the addBlog controller can now handle image files alongside blog data.

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
 import { addBlog } from "../controllers/blogController.js";
+import upload from "../middlewares/multer.js";
 
 const blogRouter = express.Router();
 
-blogRouter.post("/add", addBlog)
+blogRouter.post("/add", upload.single("image"), addBlog)
 
 export default blogRouter;
