b39f9cf62393beb6ba56a08b9669b55a4281c201
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-11 08:19:35 +0500

    Enhance blogRouter with additional routes for blog management
    - Added GET route to retrieve a specific blog by ID.
    - Implemented POST route for deleting a blog by its ID.
    - Introduced POST route to toggle the publish status of a blog.
    - Updated imports from blogController to include new functionalities.

--- server/routes/blogRoute.js
+++ server/routes/blogRoute.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { addBlog, getAllBlogs } from "../controllers/blogController.js";
+import { addBlog, deleteBlogById, getAllBlogs, getBlogById, togglePublish } from "../controllers/blogController.js";
 import upload from "../middlewares/multer.js";
 import auth from "../middlewares/auth.js";
 
@@ -7,5 +7,10 @@ const blogRouter = express.Router();
 
 blogRouter.post("/add", upload.single("image"), auth, addBlog)
 blogRouter.get("/all", getAllBlogs)
+blogRouter.get("/:blogId", getBlogById)
+blogRouter.post("/delete", auth, deleteBlogById)
+blogRouter.post("/toggle-publish", auth, togglePublish)
+
+
 
 export default blogRouter;
