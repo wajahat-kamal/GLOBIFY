0e2abefd62718794204293b41c80c7b1a95c4304
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-11 11:19:02 +0500

    Enhance comment model with additional fields and timestamps
    - Expanded the comment schema to include 'name', 'content', and 'isApproved' fields for better comment management.
    - Updated the reference for the 'blog' field to use the correct capitalization.
    - Added timestamps to track comment creation and updates.

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -1,13 +1,28 @@
 import mongoose from "mongoose";
 
-const commentSchema = new mongoose.Schema({
-   blog: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "blog",
-    required: true
-   }
-})
+const commentSchema = new mongoose.Schema(
+  {
+    blog: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog", 
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    isApproved: { 
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
-const Comment = mongoose.model("comment", commentSchema)
+const Comment = mongoose.model("Comment", commentSchema);
 
 export default Comment;
