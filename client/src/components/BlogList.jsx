7357b3c637183ae8cc9adda7fb05107d97bf211c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-13 17:11:15 +0500

    Update BlogList component to adjust top padding for improved layout

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -7,7 +7,7 @@ function BlogList() {
   const blogCategory = ["All", "Technology", "Startup", "Lifestyle"];
 
   return (
-    <section className="w-full px-4 md:px-10 py-6 ">
+    <section className="w-full px-4 md:px-10 pt-8">
       {/* Category Buttons */}
       <div className="flex flex-wrap items-center justify-center md:gap-6 gap-2 mb-6">
         {blogCategory.map((item) => (
