c9f6d91dce1bde02d73b3e19aabaaade0008400c
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-13 18:46:34 +0500

    Refactor AppContext to improve code consistency and formatting

--- client/context/AppContext.jsx
+++ client/context/AppContext.jsx
@@ -1,19 +1,21 @@
 import { createContext, useContext } from "react";
-import { useNavigate } from "react-router-dom"
-import axios from "axios"
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const AppContext = createContext();
 
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
 
-   const navigate = useNavigate();
+  
+
   const value = {};
 
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const UseAppContext = () => {
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
