c10282f229170d0c3fe807091220f8d0bfc75a17
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-13 17:30:29 +0500

    Enhance BlogList component with improved padding and max-width
    - Updated the BlogList section to include horizontal padding and a maximum width for better layout and responsiveness.
    - Ensured consistent spacing across different screen sizes for an improved user experience.

--- client/src/components/BlogList.jsx
+++ client/src/components/BlogList.jsx
@@ -27,7 +27,9 @@ function BlogList() {
         ))}
       </div>
 
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 mb-24 mx-8 md:mx-20">
+      <section
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6 mb-24 px-6 sm:px-8 lg:px-20 max-w-7xl mx-auto"
+      >
         {blogsData
           .filter((blog) => (menu === "All" ? true : blog.category === menu))
           .map((blog) => (
