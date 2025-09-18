0307903794d43e21d9d6684ddd0a58453b8dc570
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-18 17:46:06 +0500

    Improve JSX formatting in AddBlog component for enhanced readability

--- client/src/pages/admin/AddBlog.jsx
+++ client/src/pages/admin/AddBlog.jsx
@@ -164,7 +164,11 @@ function AddBlogs() {
               strokeWidth="2"
               viewBox="0 0 24 24"
             >
-              <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 9l-7 7-7-7"
+              />
             </svg>
           </div>
         </div>
