3bb2a100417c970564f908d50b435202de9009b1
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-21 18:42:14 +0500

    Update Vercel configuration to redirect all traffic to index.html and modify CORS settings to allow requests from the production URL.

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173", // Vite or CRA
+    origin: ["http://localhost:5173", "https://globify-wk.vercel.app/" ], // Vite or CRA
     credentials: true,
   })
 );
