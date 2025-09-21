9405f33d32f70f0356e9a5a1b5c66a8c46309682
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-21 12:04:32 +0500

    Add main function to gemini.js for generating AI content and logging response

--- server/configs/gemini.js
+++ server/configs/gemini.js
@@ -1,3 +1,13 @@
 import { GoogleGenAI } from  "@google/genai"
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY})
+
+
+async function main() {
+   const response = await ai.models.generateContent({
+    contents: "Explain how AI works in a few words"
+   })
+   console.log(response.text);
+   
+}
+main()
