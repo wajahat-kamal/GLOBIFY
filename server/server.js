5f608b8555fcc645a929f91576d6c59a33eba830
wajahat-kamal <wajahatkamal3.0@gmail.com>
2025-09-22 17:00:42 +0500

    Update CORS settings to allow requests from an additional local development URL for enhanced flexibility.

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://globify-wk.vercel.app" ],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://globify-wk.vercel.app" ],
     credentials: true,
   })
 );
